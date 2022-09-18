bb874f0896f295fb8f211778db4e7dccad016603
indrajitkrish <indrajitmanian@gmail.com>
2022-09-18 10:39:33 +0530

    Logic changed

--- p_or_l.js
+++ p_or_l.js
@@ -15,19 +15,21 @@ function profitorloss(){
   //console.log("comes in");
     if(cur > ip)
     {
-      var profit = (cur - ip) * qos
+      var profit = cur - ip;
       var profitpercentage = (profit / ip) * 100;
+      var total_profit = profit * qos;
       //console.log(profit);
       output.style.color = 'green';
-      output.innerText = " yup, your profit is "+ profit.toFixed(2)  +" and your profit percentage is "+profitpercentage.toFixed(2)+ "%";
+      output.innerText = " yup, your total profit is "+ total_profit.toFixed(2)  +" and your profit percentage is "+profitpercentage.toFixed(2)+ "%";
     }
    else if(ip > cur)
     {
-      var loss  = (ip - cur) * qos
+      var loss  = ip - cur;
       var losspercentage = (loss / ip) * 100;
+      var total_loss = loss * qos;
       //console.log(loss);
       output.style.color = 'red';
-      output.innerHTML = " oops :(  your loss is "+ loss.toFixed(2) +" and your loss percentage is "+losspercentage.toFixed(2)+ "%";
+      output.innerHTML = " oops :(  your total loss is "+ total_loss.toFixed(2) +" and your loss percentage is "+losspercentage.toFixed(2)+ "%";
     }
     else
     {
